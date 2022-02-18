0864e33d2779b2cfbdf7c4266a3aaf972e287377
Jota Landes <jhonatamalta@hotmail.com>
2022-02-18 17:01:20 -0300

    Start microservices program

---
+++ Api-Cliente/Models/ClienteModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Api_Cliente.Models
+{
+    public class ClienteModel
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+    }
+}
